150883c6e01e023220b92dc4cf15c8b650ec171d
Martijn Habers <martijn@habers.eu>
2025-05-02 14:39:09 +0200

    add preprocess logic

---
+++ src/lib_ml/preprocess.py
@@ -0,0 +1,30 @@
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+def preprocess(dataset, num_reviews):
+    """
+    Preprocesses the reviews.
+    
+    Parameters:
+    - dataset: pandas DataFrame, the dataset containing reviews.
+    - num_reviews: int, the number of reviews to process.
+    
+    Returns:
+    - list: A list of preprocessed reviews (corpus).
+    """
+    ps = PorterStemmer()
+    all_stopwords = stopwords.words('english')
+    all_stopwords.remove('not')
+
+    corpus = []
+
+    for i in range(num_reviews):
+        review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+        review = review.lower()
+        review = review.split()
+        review = [ps.stem(word) for word in review if word not in set(all_stopwords)]
+        review = ' '.join(review)
+        corpus.append(review)
+
+    return corpus
